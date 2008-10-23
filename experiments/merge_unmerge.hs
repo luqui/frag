ac65ab2f4ed3661a02a1a331f2ab2032c95d1782
Luke Palmer <lrpalmer@gmail.com>
2008-10-22 18:12:13 -0600

    Made it a tad more readable with a type synonym, and added a correctness check property.

--- experiments/merge_unmerge.hs
+++ experiments/merge_unmerge.hs
@@ -1,10 +1,18 @@
 
+type Unmerger = forall b. [b] -> ([b],[b])
 
-mergeUnmerge :: (Ord a) => [a] -> [a] -> ([a], forall b. [b] -> ([b],[b]))
+mergeUnmerge :: (Ord a) => [a] -> [a] -> ([a], Unmerger)
 mergeUnmerge [] bs = (bs, \r -> ([],r))
 mergeUnmerge as [] = (as, \r -> (r,[]))
 mergeUnmerge (a:as) (b:bs)
-    | a <= b    = let (rest, unm :: forall b. [b] -> ([b],[b])) = mergeUnmerge as (b:bs) 
+    | a <= b    = let (rest, unm :: Unmerger) = mergeUnmerge as (b:bs) 
                   in (a:rest, \ (a':rest') -> let (as',bs') = unm rest' in (a':as',bs'))
-    | otherwise = let (rest, unm :: forall b. [b] -> ([b],[b])) = mergeUnmerge (a:as) bs
+    | otherwise = let (rest, unm :: Unmerger) = mergeUnmerge (a:as) bs
                   in (b:rest, \ (b':rest') -> let (as',bs') = unm rest' in (as',b':bs'))
+
+prop_inverses xs ys = 
+    let (combined, unmerge :: Unmerger) = mergeUnmerge xs ys
+        (xs',ys')                       = unmerge combined
+    in xs == xs' && ys == ys'
+    where
+    types = (xs :: [Integer])
