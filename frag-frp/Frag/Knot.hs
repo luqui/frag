828847bad433ad07633d02725cce151a85c33400
Luke Palmer <lrpalmer@gmail.com>
2008-10-23 17:24:17 -0600

    Okay, Knot is, um, "working", for some very liberal definition of that word. Going to delete it.  Why must this be so hard!!

--- frag-frp/Frag/Knot.hs
+++ frag-frp/Frag/Knot.hs
@@ -1,22 +1,51 @@
-module Frag.Knot ( Knot, write, extract, runKnot ) where
+module Frag.Knot where
 
-import MonadLib
-import Data.Monoid
+import Data.Monoid hiding (Any)
+import qualified Frag.NatTrie as Trie
+import GHC.Prim (Any)
+import Unsafe.Coerce
+import Control.Monad
+import Control.Applicative
+import Data.Maybe
 
-newtype Knot w a = Knot (WriterT w (StateT w Id) a)
-    deriving (Functor,Monad)
+newtype ID a b = ID Integer
 
-write :: (Monoid w) => w -> Knot w ()
-write = Knot . put
+type Satisfier = Trie.NatTrie (Maybe Any)
 
-extract :: (Monoid w) => (w -> (a,w)) -> Knot w a
-extract f = Knot $ do
-    w <- get
-    let (x,w') = f w
-    set w'
-    return x
+data Knot :: * -> * where
+    Return  :: Satisfier -> a -> Knot a
+    Request :: ID b c -> b -> (c -> Knot a) -> Knot a
 
-runKnot :: Knot w a -> a
-runKnot (Knot m) = let ((x,_),w) = runId (runStateT w (runWriterT m)) in x
-                       --     ^---------------------^
-                       --          tying the knot
+mergeTries :: Satisfier -> Satisfier -> Satisfier 
+mergeTries = liftA2 mplus
+
+addTo :: Satisfier -> Knot a -> Knot a
+addTo sat (Return sat' x) = Return (mergeTries sat sat') x
+addTo sat (Request req inp cont) = Request req inp (addTo sat . cont)
+
+instance Functor Knot where
+    fmap f (Return t a) = Return t (f a)
+    fmap f (Request id b cont) = Request id b (fmap f . cont)
+
+instance Monad Knot where
+    return = Return (Trie.uniform Nothing)
+    Return trie a >>= f = addTo trie (f a)
+    Request req inp cont >>= f = Request req inp (cont >=> f)
+
+runKnot' :: Knot a -> (Satisfier, a)
+runKnot' (Return sat x) = (sat,x)
+runKnot' (Request (ID req) inp cont) = 
+    let (sat,x)           = runKnot' (cont outp)
+        (sat',(outp,kf')) = runKnot' (runProcessor (unsafeCoerce (fromJust (Trie.lookup req sat''))) inp)
+        sat''             = sat -- XXX must be wrong!
+    in (Trie.insert req (Just (unsafeCoerce kf')) sat'', x)
+
+runKnot :: Knot a -> a
+runKnot = snd . runKnot' 
+
+newtype Processor a b = Processor { runProcessor :: a -> Knot (b, Processor a b) }
+
+nonlocal :: Integer -> Processor a b -> Knot (a -> Knot b)
+nonlocal ident proc = Return sat (\b -> Request (ID ident) b return)
+    where
+    sat = Trie.insert ident (Just (unsafeCoerce proc)) (Trie.uniform Nothing)
