b170aabe9558c4fe260ae779b4813986a659d7b2
Luke Palmer <lrpalmer@gmail.com>
2008-10-26 17:14:53 -0600

    Added introduction and elimination functions.

--- frag-frp/Frag/FutureST.hs
+++ frag-frp/Frag/FutureST.hs
@@ -1,4 +1,6 @@
-module Frag.FutureST where
+module Frag.FutureST 
+    ( FutureST, future, computation, runFutureST )
+where
 
 import Control.Monad.ST.Lazy
 import Frag.Time
@@ -23,3 +25,22 @@ instance Monad (FutureST r) where
         where
         bind (Exact x) = unFutureST (f x)
         bind (Next st) = return (Next (fmap (>>= f) st))
+
+instance Applicative (FutureST r) where
+    pure = return
+    (<*>) = ap
+
+future :: Future a -> FutureST r a
+future = FutureST . fmap Exact
+
+computation :: Future (ST r (FutureST r a)) -> FutureST r a
+computation = FutureST . fmap Next
+
+commute :: Future (ST r a) -> ST r (Future a)
+commute fut = fmap (makeFuture (time fut)) (value fut)
+
+runFutureST :: FutureST r a -> ST r (Future a)
+runFutureST (FutureST m) = fmap join (commute (fmap go m))
+    where
+    go (Exact a) = return (return a)
+    go (Next st) = runFutureST =<< st
