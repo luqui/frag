1d4648e77e05794b7f0d8686985ab867b819dacf
Luke Palmer <lrpalmer@gmail.com>
2008-12-03 22:16:10 -0700

    A bunch of new combinators for Reactive.

--- frag-frp/Frag/Reactive.hs
+++ frag-frp/Frag/Reactive.hs
@@ -1,6 +1,7 @@
 module Frag.Reactive 
     ( Reactive
-    , until, snapshot
+    , until, stepper, snapshot
+    , accumWith, accum, change
     )
 where
 
@@ -9,6 +10,7 @@ import Frag.Future
 import Control.Applicative
 import Control.Monad
 import Control.Comonad
+import Data.Monoid (Monoid, mappend, mempty)
 
 data Reactive a = Reactive a (Future (Reactive a))
 
@@ -33,6 +35,20 @@ instance Comonad Reactive where
 until :: Reactive a -> Future (Reactive a) -> Reactive a
 until (Reactive x xs) fut = Reactive x (xs `mplus` fut)
 
+stepper :: a -> Future (Reactive a) -> Reactive a
+stepper = Reactive
+
 snapshot :: Reactive a -> Future b -> Future (a,b)
 snapshot (Reactive x fxs) fut = 
      fmap ((,) x) fut `mplus` (fxs >>= \xs -> snapshot xs fut)
+
+accumWith :: (a -> a -> a) -> a -> Reactive a -> Reactive a
+accumWith append zero (Reactive x xs) = Reactive cur (accumWith append cur <$> xs)
+    where
+    cur = zero `append` x
+
+accum :: (Monoid a) => Reactive a -> Reactive a
+accum = accumWith mappend mempty
+
+change :: Reactive a -> Future (Reactive a)
+change (Reactive x xs) = xs
