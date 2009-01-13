abf53dc938d61917f62ff1bf1cc37d2df0e67549
Luke Palmer <lrpalmer@gmail.com>
2009-01-12 20:57:19 -0700

    Documented Event's semantics.

--- frag-frp/Frag/Event.hs
+++ frag-frp/Frag/Event.hs
@@ -18,6 +18,13 @@ newtype PrimMap a = PrimMap (Map.Map Unique (Any -> a))
 instance Functor PrimMap where
     fmap f (PrimMap m) = PrimMap ((fmap.fmap) f m)
 
+-- | @Event a@ is conceptually an event that can occur.
+--
+-- Semantics: @Event a = Time -> (Time,a)@; where the
+-- returned time is no less than the argument.  Intuitively,
+-- \"wait some amount of time and yield a value\".
+--
+-- Functor, Monad, Applicative are all the same as State.
 data Event a
     = Return a
     | Wait [PrimMap (Event a)]
@@ -36,8 +43,22 @@ instance Applicative Event where
     pure = return
     (<*>) = ap
 
+-- | An event that never occurs. 
+--
+-- Semantics: @never t = (infinity, undefined)@
 never :: Event a
 never = Wait []
+
+-- | The first of two events to occur.
+--
+-- Semantics: 
+--
+-- > firstOf e e' t0 = minBy (comparing fst) (e t0) (e' t0)
+-- >     where
+-- >     minBy f x y = case f x y of
+-- >                       LT -> x
+-- >                       EQ -> x
+-- >                       GT -> y
 
 firstOf :: Event a -> Event a -> Event a
 firstOf (Return x) _ = Return x
